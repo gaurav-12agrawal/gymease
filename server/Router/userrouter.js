33c7f4fd3c9cecc07112428ebee8b3518ddb4af9
gaurav-12agrawal <108812877+gaurav-12agrawal@users.noreply.github.com>
2023-05-29 09:50:25 +0530

    consol.logtoken

--- server/Router/userrouter.js
+++ server/Router/userrouter.js
@@ -114,8 +114,10 @@ userrouter.post('/signin', async (req, res) => {
                 const token = await userLogin.generateAuthToken();
                 res.cookie('jwtoken', token, {
                     expires: new Date(Date.now() + 25892000000),
-                    sameSite: "none"
+                    sameSite: "none",
+                    secure: true
                 })
+                console.log(token)
                 return res.status(200).json({ message: "Login successfully" })
 
             }
