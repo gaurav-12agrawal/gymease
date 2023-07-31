fc2fd513f0b700d8c29c5227e18bf6545caea8cb
gaurav-12agrawal <108812877+gaurav-12agrawal@users.noreply.github.com>
2023-07-31 17:13:12 +0530

    forget password gmail update

--- server/Router/userforrouter.js
+++ server/Router/userforrouter.js
@@ -42,7 +42,34 @@ userforrouter.post("/sendpasswordlink", async (req, res) => {
                 from: process.env.MAIL_USERNAME_VERIFY,
                 to: email,
                 subject: "Sending Email For password Reset",
-                text: `This Link Valid For 2 MINUTES https://gymozy.com/forgetpassword/${userfind._id}/${setusertoken.verifytoken}`
+                html: `
+                <div  style="display:flex;justify-content:center;align-items:center; margin:auto;" >
+                <img  alt="Image" style="width: 50%; height: 50%; margin:auto;  border-radius:100%;" src= 'https://res.cloudinary.com/dgfn40mfc/image/upload/v1685128198/Important%20image/logo_zzpwsu.jpg' ></img></div> 
+                 <br/><br/><br/>
+                <div style="width: 100%; height: 90px; background-color:green;display:flex;
+                justify-content:center;align-items:center; margin:auto;" ><p style="color:white; font-size:20px ;margin:auto;" >Password Reset Link</p></div>
+                <p>We hope this email finds you in good health and high spirits. It seems like you've forgotten your Gymozy account password. No worries, we are here to assist you in regaining access to your account.
+                </p>
+                <p>To reset your password, please click on the link provided below. <b>Please note that this link will be valid for the next 2 minutes for security purposes:</b>
+                </p>
+                <a href="https://gymozy.com/forgetpassword/${userfind._id}/${setusertoken.verifytoken}" >PassWord Reset Link</a><br/>
+                <p>
+                By clicking on the link, you will be directed to a secure page where you can reset your Gymozy account password. If you did not initiate this password reset, kindly ignore this email. Rest assured that your account is safe and secure.</br></br>
+                
+                For security reasons, we recommend that you choose a strong and unique password that you have not used for any other service.</br></br>
+                
+                If you encounter any issues during the password reset process or have any concerns regarding your account, please don't hesitate to contact our support team at support.gymozy@gmail.com . We are here to assist you and ensure a smooth experience.</br></br>
+                
+                Thank you for choosing Gymozy as your fitness partner. We value your trust and strive to provide the best service possible.</br></br>
+                
+                Best regards,</br></br>
+                
+                The Gymozy Team
+                </p>
+
+                       
+                       
+                `
             }
 
             transporter.sendMail(mailOptions, (error, info) => {
