648d3576d6147013bc6b5de738d25a8fd8914137
gaurav-12agrawal <108812877+gaurav-12agrawal@users.noreply.github.com>
2023-08-31 15:13:11 +0530

    changing loader

--- client/src/components/Screenloader.js
+++ client/src/components/Screenloader.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import HashLoader from "react-spinners/HashLoader";
-
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 const Screenloader = () => {
+    useEffect(() => {
+        AOS.init({ duration: 1200 })
+    }, [])
     return (
-        <div className='loadingdiv'>
-            <HashLoader
-                color={"#36d7b7"}
-
-            />
+        <div className='loadingdiv' data-aos="zoom-in">
+            <img src='https://res.cloudinary.com/dgfn40mfc/image/upload/v1690913288/Important%20image/GYMOZY_LOGO_llplc3.png' alt="logo" className='loading-div-image' />
         </div>
     )
 }
