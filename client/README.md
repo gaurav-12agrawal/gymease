21cbc28cfcffc3bde92d7eac385a7afd89701eb0
Gaurav Agrawal <108812877+gaurav-12agrawal@users.noreply.github.com>
2024-09-20 16:08:11 +0530

    changes

--- client/README.md
+++ client/README.md
@@ -1,1 +1,4 @@
 # BOOK
+                {/* <Route path="/aboutus" element={<Aboutus />} /> */}
+                {/* <Route path="/faqpage" element={<Faq />} /> */}
+                {/* <Route path="/privacyandpolicy" element={<Privacy />} /> */}
--- client/src/components/Checkoutform.js
+++ client/src/components/Checkoutform.js
@@ -221,9 +221,9 @@ const Checkoutform = () => {
                         <p className={Checkoutcss.note}>Make sure , you fill payment form given below</p>
                         <div className={Checkoutcss.upiqrdiv}>
                             <p className={Checkoutcss.qrheading}>Scan me to pay</p>
-                            <img className={Checkoutcss.qrimage} src='https://res.cloudinary.com/dgfn40mfc/image/upload/v1693473304/Important%20image/rinesh_qr_bbeqne.jpg'></img>
+                            <img className={Checkoutcss.qrimage} src='https://res.cloudinary.com/dgfn40mfc/image/upload/v1726828626/0984c311-dc65-4ac0-ab9f-901a9c2437f6.png'></img>
                             <p className={Checkoutcss.upiheading}>Or make a upi payment </p>
-                            <p className={Checkoutcss.upi}><b>gymozy@ybl</b></p>
+                            <p className={Checkoutcss.upi}><b>gauravagrawal1202@axl</b></p>
                         </div>
                     </div>
                     <div className={Checkoutcss.form}>
