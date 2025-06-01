95e4cd34b84d5e4635d665e701166d8314796e0e
Gaurav Agrawal <gauravagrawal1202@gmail.com>
2025-06-01 22:44:00 +0530

    changes for progressive webapp

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
@@ -15,7 +14,4 @@ root.render(
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.register();
